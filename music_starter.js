8c00861301fc583b97a98759f6ae97bea5681091
Zekegamby <zekegamby@gmail.com>
2023-10-02 10:29:25 +1300

    changes

--- music_starter.js
+++ music_starter.js
@@ -4,6 +4,8 @@ let backgrounImage;
 let trumpetImage;
 let trumpetImage1;
 let trumpetImage2;
+let notes = [];
+let noteSpawnRate = 60;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 
 
@@ -33,6 +35,11 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       trumpetImage = loadImage('trumpet.png');
       trumpetImage1 = loadImage('trumpet.png');
       trumpetImage2 = loadImage('trumpet.png');
+      bassImage = loadImage('bass.png');
+      redMusicImage = loadImage('redMusic.png');
+      greenMusicImage = loadImage('greenMusic.png');
+      blueMusicImage = loadImage('blueMusic.png');
+      rainbowMusicImage = loadImage('rainbowMusic.png');
       firstRun = false;
     }
 
@@ -49,11 +56,70 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     push();
     shadow(20);
     translate(300,800)
-    let newDrum = map(drum, 0, 100, -50, 50, true);
-    image(drumImage, newDrum, newDrum/2);
+    let newDrum = map(drum, 0, 100, 1, 2, true);
+    if (drum >= 50){
+      //drumImage.resize(drum + 500, drum/4 + 500);
+  
+      scale(newDrum - .5, newDrum - .5)
+      }
+    image(drumImage, 0, 0);
+
     print(newDrum);
     pop();
 
+
+    //Bass
+    push()
+    translate(500, 600);
+    let newBass = map(bass, 0, 100, 1, 1.3, true);
+    let newNotes = map(bass, 0, 100, -50, 50, true);
+    push();
+    scale(1, newBass);
+    image(bassImage, bass/4, bass/4);
+    pop()
+    
+    if (bass > 60 && frameCount % noteSpawnRate === 0) {
+      // Generate a random x-coordinate around the bass player
+      let randomX = bass + random(-50, 100); // Adjust the range based on your preference
+  
+      // Generate a random note
+      let note = {
+        x: randomX,
+        y: bass - 100,
+        speed: random(2, 5), // Adjust the speed of the note
+        image: randomNoteImage(), // Function to get a random note image
+        size: random(0.1, 0.5) // Adjust the range for smaller sizes
+      };
+  
+      // Add the note to the array
+      notes.push(note);
+    }
+  
+    // Display and move notes
+    for (let i = notes.length - 1; i >= 0; i--) {
+      let note = notes[i];
+  
+      // Scale the note image based on the size property
+      let scaledWidth = note.image.width * note.size;
+      let scaledHeight = note.image.height * note.size;
+      image(note.image, note.x - scaledWidth / 2, note.y - scaledHeight / 2, scaledWidth, scaledHeight);
+  
+      // Move the note upwards
+      note.y -= note.speed;
+  
+      // Remove notes that go off-screen
+      if (note.y < -600) {
+        notes.splice(i, 1);
+      }
+    }
+
+  
+  // Function to get a random note image
+  function randomNoteImage() {
+    let images = [redMusicImage, greenMusicImage, blueMusicImage, rainbowMusicImage];
+    return random(images);
+  }
+  pop()
 
 
     //Trumpet
@@ -61,7 +127,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     shadow(20);
     translate(1500, 800);
     let newTrumet = map(other, 0, 100, -50, 50, true);
-    let newTrumetRotate = map(other, 0, 100, -35, 50, true);
+    let newTrumetRotate = map(other, 0, 100, -50, 50, true);
     let newTrumetColor = map(other, 0, 100, -50, 100, true);
     if(other > 65){
       push()
@@ -77,39 +143,9 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   }
     push()
     if(other>70){
-      rotate(newTrumetRotate)
+      rotate((other - 70) * 2.5)
     }
-    // if(other>69){
-    //   rotate(2)
-    // }
-    // if(other>70){
-    //   rotate(2)
-    // }
-    // if(other>71){
-    //   rotate(1)
-    // }
-    // if(other>72){
-    //   rotate(1)
-    // }
-    // if(other>73){
-    //   rotate(1)
-    // }
-    // if(other>74){
-    //   rotate(1)
-    // }
-    // if(other>75){
-    //   rotate(1)
-    // }
-    // if(other>80){
-    //   rotate(5)
-    // }
-    // if(other>85){
-    //   rotate(5)
-    // }
-    // if(other>90){
-    //   rotate(5)
-    //
-    // }
+
     if(other > 65){
         image(trumpetImage, newTrumet, newTrumet/2)
         pop()
@@ -130,7 +166,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
        
     }
     pop()
-    if (other > 65){
+    if (other > 70){
       translate(1500, 800);
       noStroke()
     fill(320, 350, 0, newTrumetColor);
@@ -178,4 +214,4 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   //  textAlign(CENTER);
   //  textSize(vocal);
   //  text(words, width/2, height/3);
-}
+  }
